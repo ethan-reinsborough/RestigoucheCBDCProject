503f7c2b0d2f38301e8d8ce197e74a70e4ef53ea
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-08-10 16:55:35 -0300

    Created Cover class
    The Cover class will hold information pertaining to the optional image added to a book.

---
+++ BookApplication/BookApplication/Models/Cover.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookApplication.Models
+{
+    public class Cover
+    {
+        [Display(Name = "CoverID")]
+        public int ID { get; set; }
+
+        [StringLength(150, MinimumLength = 2)]
+        [Required(ErrorMessage = "Cover Id is required.")]
+        public string imageSelected { get; set; }
+
+        [Required(ErrorMessage = "Print Date is required.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        public DateTime printDate { get; set; }
+
+        public virtual ICollection<File> Files { get; set; }
+
+        public Book Book { get; set; }
+    }
+}
