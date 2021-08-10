1f3f2ed41834506a1d59654165a7ea43cd53d0c0
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-08-10 16:52:12 -0300

    Created Book class
    Created a Book entity which is comprised of a generated ID, title, publication date, and a cover (optional book image). Updated connection string to use a SQL DB.

---
+++ BookApplication/BookApplication/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BookApplication.Models
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = false)]
+        [Display(Name = "Publication Date")]
+
+        public DateTime PublicationDate { get; set; }
+
+        public virtual Cover Cover { get; set; }
+    }
+}
