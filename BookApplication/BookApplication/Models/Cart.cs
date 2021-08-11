8a991aef09d6324bb83fc24e0630df74049e0514
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-08-11 17:42:28 -0300

    Added sorting by title and publication date. Beginning user book list

---
+++ BookApplication/BookApplication/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookApplication.Models
+{
+    public class Cart
+    {
+        //Will be the logged in user's ID from AspNetUsers table
+        public string ID { get; set; }
+
+
+
+    }
+}
