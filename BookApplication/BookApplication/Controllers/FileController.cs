806c5474eee747ec42ce0018ab1445992574ff99
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-08-11 16:36:34 -0300

    Finished image upload
    Fixed bugs involving image upload, added a file controller to handle image retrieval from the database.

---
+++ BookApplication/BookApplication/Controllers/FileController.cs
@@ -0,0 +1,21 @@
+﻿using BookApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookApplication.Controllers
+{
+    public class FileController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: File
+        public ActionResult Index(int id)
+        {
+            var fileToRetrieve = db.Files.Find(id);
+            return File(fileToRetrieve.Content, fileToRetrieve.ContentType);
+        }
+    }
+}
